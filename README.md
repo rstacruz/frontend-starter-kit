eab7f4073cfa7928bbbcf97a5fe997840b8820b6
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-08-28 14:30:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# frontend-starter-kit
+
+A small project that takes a bunch of HTML (`web/`), CSS (`css/`), JS files (`js/`) and turns them into a website (`public/`).
+
+Included in the kit:
+
+- Build pipeline (via [Metalsmith](http://metalsmith.io))
+- Development server (via [metalsmith-start](https://www.npmjs.com/package/metalsmith-start))
+- Browserify
+- Babel (with stage0 and react JSX)
+- Sass + autoprefixer
+- Jade/pug support
+- Production compression
+
+## Usage
+
+Run in development (or production):
+
+```
+npm start
+```
+
+Build:
+
+```
+npm run build
+```
---
+++ metalsmith.js
@@ -0,0 +1,27 @@
+var Metalsmith = require('metalsmith')
+
+var b = require('metalsmith-browserify')('assets/app.js', {
+  entries: ['js/app.js'],
+  cache: {},
+  packageCache: {}
+})
+b.bundle.transform('babelify')
+
+if (process.env.NODE_ENV === 'production') {
+  b.bundle.transform({ global: true }, 'uglifyify')
+} else {
+  b.bundle.plugin('watchify')
+}
+
+var app = Metalsmith(__dirname)
+  .source('./web')
+  .destination('./public')
+  .use(require('metalsmith-jstransformer')())
+  .use(require('metalsmith-sense-sass')())
+  .use(b)
+
+if (module.parent) {
+  module.exports = app
+} else {
+  app.build(function (err) { if (err) throw err })
+}
