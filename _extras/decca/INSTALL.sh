aa113c6b88bde1337dc3150f27db84067cece23e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-08-28 20:13:04 +0800

    Update INSTALL.sh

--- _extras/decca/INSTALL.sh
+++ _extras/decca/INSTALL.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
 curl -sSL https://github.com/rstacruz/frontend-starter-kit/archive/master.tar.gz \
- | tar zxv --strip-components=3 '*/_extras/decca' \
- --exclude='INSTALL.sh'
+ | tar zxv --strip-components=3 \
+ --exclude='INSTALL.sh' \
+ '*/_extras/decca'
 
 npm install --cache-min Infinity --save --save-exact \
   riot-route \
